fe6849e9a74401c1ac1a819c8c451048ad40e4bb
Snigdha Tiwari <snigdha2210@gmail.com>
2020-07-18 13:18:38 +0530

    Updates

---
+++ precache-manifest.a9ac3ee7c7c14e1aff7e6d7c377b04b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2f058f48ec796023851abac15c7b51",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "b322ddeb53dd9be99801",
+    "url": "/pics/static/css/main.310e5213.chunk.css"
+  },
+  {
+    "revision": "e33d29e69669432efcf3",
+    "url": "/pics/static/js/2.58488f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics/static/js/2.58488f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b322ddeb53dd9be99801",
+    "url": "/pics/static/js/main.0b1ab21d.chunk.js"
+  },
+  {
+    "revision": "24445b424d0ed337bcc7",
+    "url": "/pics/static/js/runtime-main.db0de8af.js"
+  }
+]);
